f16c59bed9b4fffbcc82ed567d7e4dedee9c6f9e
Sushant-Singh-Rathore <ssr7705@gmail.com>
2020-05-16 17:02:19 +0530

    initial commit

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,7 +5,7 @@ public class Main {
     public static void main(String[] args) {
 	// write your code here
 
-        SiddharthPhone phone = new SuryanshPhone();
+        SuryanshPhone phone = new SuryanshPhone();
         phone.call();
         phone.cook();
         phone.dance();
